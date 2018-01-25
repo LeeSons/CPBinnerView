9b0b04db99915f2fa98c13b90bd158f63abd4943
孙登峰 <oneleaf@sundengfengdemacbook-pro.local>
2018-01-25 13:25:15 +0800

    Initial Commit

---
+++ CPBinnerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPBinnerViewDemo
+//
+//  Created by 孙登峰 on 2018/1/25.
+//  Copyright © 2018年 zentcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CPBinnerViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CPBinnerViewDemo
+//
+//  Created by 孙登峰 on 2018/1/25.
+//  Copyright © 2018年 zentcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
